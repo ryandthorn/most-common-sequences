5717b542d83ae23fe189aae8fdf857aa901a95b0
Ryan Thorn <ryandthorn@gmail.com>
2022-03-13 12:01:36 -0700

    Add load calls to worker with debug output

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	if err := mostCommonSequences(); err != nil {
@@ -9,5 +12,20 @@ func main() {
 }
 
 func mostCommonSequences() error {
+	files, err := loadFiles(os.Args[1:])
+	if err != nil {
+		return fmt.Errorf("error loading files from args: %w", err)
+	}
+
+	fromStdin, err := loadStdin()
+	if err != nil {
+		return fmt.Errorf("error loading text from stdin: %w", err)
+	}
+
+	if fromStdin != nil {
+		files = append(files, fromStdin)
+	}
+
+	fmt.Printf("files loaded: %d\n", len(files))
 	return nil
 }
