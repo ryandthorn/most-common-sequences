9a687a5f68454734d1e262e2b785f562cdc995ac
Ryan Thorn <ryandthorn@gmail.com>
2022-03-22 21:14:30 -0700

    Create server with test handlers

--- main.go
+++ main.go
@@ -2,49 +2,57 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"net/http"
+	"strings"
 )
 
 func main() {
-	if err := mostCommonSequences(); err != nil {
-		fmt.Println(err)
-	}
+	http.HandleFunc("/test/simple", handleTestSimple)
+	http.HandleFunc("/test/full", handleTestFull)
+	http.ListenAndServe(":8080", nil)
 }
 
-func mostCommonSequences() error {
-	files, err := loadFiles(os.Args[1:])
+func handleTestSimple(w http.ResponseWriter, req *http.Request) {
+	f, err := mostCommonSequences("testfiles/simple.txt")
 	if err != nil {
-		return fmt.Errorf("error loading files from args: %w", err)
+		fmt.Fprintf(w, err.Error())
 	}
-
-	fromStdin, err := loadStdin()
-	if err != nil {
-		return fmt.Errorf("error loading text from stdin: %w", err)
-	}
-
-	if fromStdin != nil {
-		files = append(files, fromStdin)
-	}
-
-	for _, f := range files {
-		wgs, err := processFile(f)
-		if err != nil {
-			return fmt.Errorf("error processing file contents: %w", err)
-		}
-		f.SetTop100(wgs)
-	}
-
-	printMostCommonSequences(files)
-
-	return nil
+	fmt.Fprint(w, stringifyMostCommonSequences(f))
 }
 
-func printMostCommonSequences(files []*file) {
-	for _, f := range files {
-		fmt.Printf("\nResults for %s\n", f.Name())
-		fmt.Println("---------------------------------")
-		for _, wg := range f.GetTop100() {
-			fmt.Printf("%s: %d\n", wg.Text(), wg.Count())
-		}
+func handleTestFull(w http.ResponseWriter, req *http.Request) {
+	f, err := mostCommonSequences("testfiles/darwin-full.txt")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
 	}
+	fmt.Fprint(w, stringifyMostCommonSequences(f))
+}
+
+func mostCommonSequences(filepath string) (*file, error) {
+	f, err := loadFile(filepath)
+	if err != nil {
+		return nil, fmt.Errorf("error loading file: %w", err)
+	}
+
+	wgs, err := processFile(f)
+	if err != nil {
+		return nil, fmt.Errorf("error processing file contents: %w", err)
+	}
+	f.SetTop100(wgs)
+
+	return f, nil
+}
+
+func printMostCommonSequences(f *file) {
+	fmt.Print(stringifyMostCommonSequences(f))
+}
+
+func stringifyMostCommonSequences(f *file) string {
+	b := strings.Builder{}
+	b.WriteString(fmt.Sprintf("\nResults for %s\n", f.Name()))
+	b.WriteString("---------------------------------\n")
+	for _, wg := range f.GetTop100() {
+		b.WriteString(fmt.Sprintf("%s: %d\n", wg.Text(), wg.Count()))
+	}
+	return b.String()
 }
